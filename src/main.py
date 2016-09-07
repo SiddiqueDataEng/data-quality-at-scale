bb5c367f3d26ba5ca6828ba895f7ef02e97b7e2c
Siddique Data Engineer <dba.siddique@gmail.com>
2016-09-07 13:35:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+65 Data Quality At Scale - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 65-data-quality-at-scale")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
